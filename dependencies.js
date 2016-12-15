04bf8ced9ffdbac3c6986f597c801af732871fca
chrisdraws <chrisdraws@gmail.com>
2016-12-14 21:58:12 -0500

    Update dependencies.js

--- dependencies.js
+++ dependencies.js
@@ -6,43 +6,38 @@ let acorn = require('acorn'),
   resultsObject = {},
   returnedValue = "This file contained no dependent modules.";
 
-
-
-  fs.readFile(process.argv[2],'utf8',function(err, results){
+fs.readFile(process.argv[2],'utf8',function(err, results){
 
     newValue = acorn.parse(results).body;
 
+    for (var key in newValue) {
+      if (newValue.hasOwnProperty(key)) {
 
+        //console.log(newValue[key]);
+        //is this a variable declaration? or is it something else
+        if(newValue[key].type === 'VariableDeclaration') {
+          newValue[key].declarations.forEach(function(value){
 
+            //console.log(key, newValue[key]);
+            //does this have a callee attached to it and is that callee a require();
 
-       for (var key in newValue) {
-          if (newValue.hasOwnProperty(key)) {
+            if(value.init !== null && value.init.callee && value.init.callee.name ==="require"){
 
-            //console.log(newValue[key]);
-            //is this a variable declaration? or is it something else
-            if(newValue[key].type === 'VariableDeclaration') {
-              newValue[key].declarations.forEach(function(value){
-                //console.log(key, newValue[key]);
-                //does this have a callee attached to it and is that callee a require();
-
-                if(value.init !== null && value.init.callee && value.init.callee.name ==="require"){
-                  //add the value to the object
-                  resultsObject[value.id.name] = value.init.arguments[0].value;
-                } else {
-                  //do nothing
-                }
-              });
+              //insert the value to the object
+                resultsObject[value.id.name] = value.init.arguments[0].value;
             } else {
-              // do nothing
+                //do nothing
             }
-          }
+          });
+        } else {
+            // do nothing
         }
-        if(JSON.stringify(resultsObject, null, 2) !== "{}") {
-          returnedValue = JSON.stringify(resultsObject, null, 2);
-        }
+      }
+    }
+    if(JSON.stringify(resultsObject, null, 2) !== "{}") {
+        returnedValue = JSON.stringify(resultsObject, null, 2);
+    }
 
-console.log(returnedValue);
-
-
+    console.log(returnedValue);
 
 });
