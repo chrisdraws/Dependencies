b639ae7a11994f299b58d167b848b2e23a9c714f
chrisdraws <chrisdraws@gmail.com>
2016-12-14 21:53:19 -0500

    Initial commit

---
+++ dependencies.js
@@ -0,0 +1,48 @@
+"use strict"
+
+let acorn = require('acorn'),
+  fs = require('fs'),
+  newValue,
+  resultsObject = {},
+  returnedValue = "This file contained no dependent modules.";
+
+
+
+  fs.readFile(process.argv[2],'utf8',function(err, results){
+
+    newValue = acorn.parse(results).body;
+
+
+
+
+       for (var key in newValue) {
+          if (newValue.hasOwnProperty(key)) {
+
+            //console.log(newValue[key]);
+            //is this a variable declaration? or is it something else
+            if(newValue[key].type === 'VariableDeclaration') {
+              newValue[key].declarations.forEach(function(value){
+                //console.log(key, newValue[key]);
+                //does this have a callee attached to it and is that callee a require();
+
+                if(value.init !== null && value.init.callee && value.init.callee.name ==="require"){
+                  //add the value to the object
+                  resultsObject[value.id.name] = value.init.arguments[0].value;
+                } else {
+                  //do nothing
+                }
+              });
+            } else {
+              // do nothing
+            }
+          }
+        }
+        if(JSON.stringify(resultsObject, null, 2) !== "{}") {
+          returnedValue = JSON.stringify(resultsObject, null, 2);
+        }
+
+console.log(returnedValue);
+
+
+
+});
