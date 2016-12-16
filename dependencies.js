dad16c2d20593f2f6f90244bc5f4d264749bfcc3
chrisdraws <chrisdraws@gmail.com>
2016-12-15 22:51:30 -0500

    switched from for in loop to using acorns walk to explore the tree and return the discovered dependencies

--- dependencies.js
+++ dependencies.js
@@ -1,6 +1,7 @@
 "use strict"
 
 let acorn = require('acorn'),
+  walk = require('acorn/dist/walk'),
   fs = require('fs');
 
 
@@ -15,36 +16,24 @@ fs.readFile(process.argv[2],'utf8',function(err, results){
     return;
   } else {
 
-    newValue = acorn.parse(results,{"allowHashBang":"true"}).body;
+    newValue = acorn.parse(results,{"allowHashBang":"true"});
 
-    for (var key in newValue) {
-      if (newValue.hasOwnProperty(key)) {
+    walk.simple(newValue,{
 
-        //console.log(newValue[key]);
-        //is this a variable declaration? or is it something else
-        if(newValue[key].type === 'VariableDeclaration') {
-          newValue[key].declarations.forEach(function(value){
+      VariableDeclarator: function(node){
+        if(node.init.callee && node.init.callee.name === "require") {
 
-            //console.log(key, newValue[key]);
-            //does this have a callee attached to it and is that callee a require();
-
-            if(value.init !== null && value.init.callee && value.init.callee.name ==="require"){
-
-              //insert the value to the object
-                resultsObject[value.id.name] = value.init.arguments[0].value;
-            } else {
-                //do nothing
-            }
-          });
-        } else {
-            // do nothing
+         resultsObject[node.id.name] = node.init.arguments[0].value;
         }
       }
-    }
+    })
     if(JSON.stringify(resultsObject, null, 2) !== "{}") {
         returnedValue = JSON.stringify(resultsObject, null, 2);
     }
-  }
+
     console.log(returnedValue);
+    //newValue = newValue.body;
+}
+
 
 });
